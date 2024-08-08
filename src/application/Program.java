82d698fd1d7257aabeedd7173bdcd1a9c61b5c40
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2024-08-08 09:26:18 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(5, "Felipe");
+		System.out.println(dp);
+	}
+
+}
