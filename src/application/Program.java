5d8833d51f66782196e36d685229b1f3b461880f
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2024-08-09 10:52:09 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(5, "Felipe");
-		System.out.println(dp);
-		
-		Seller sl = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println(sl);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
